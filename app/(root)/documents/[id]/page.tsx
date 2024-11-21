aec7095f26ba4fe4ad04de8e4ab074d2e37c5a51
Akhil Kumar <75962924+AKHILKAP124@users.noreply.github.com>
2024-11-21 12:34:18 +0530

    add: add header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,18 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 const Document = () => {
   return (
     <>
-    <div>Document</div>
+      <Header>
+        <div className='flex w-fit items-center justify-between gap-2'>
+          <p className='document-title'>
+            skjfsklnfj
+          </p>
+        </div>
+      </Header>
+        
       <Editor />
     </>
   )
